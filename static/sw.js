a94aba657cbb740ad5964c95d38c901afb39bbdf
Jundi Mubarok <57140394+jundi14@users.noreply.github.com>
2023-11-05 05:16:42 +0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,56 +1,18 @@
-// sw.js
+// This is the "Offline copy of pages" service worker
 
-// Nama cache untuk menyimpan sumber daya website
-const CACHE_NAME = 'cool-cache';
+const CACHE = "pwabuilder-offline";
 
-// Daftar sumber daya yang akan disimpan di cache
-const urlsToCache = [
-  '/assets'
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener('install', function(event) {
-  // Melakukan instalasi service worker dan menyimpan sumber daya di cache
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener('fetch', function(event) {
-  // Mengecek apakah sumber daya tersedia di cache, jika tidak, memuatnya dari server
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Jika ada respons di cache, kembalikan dari cache
-        if (response) {
-          return response;
-        }
-        // Jika tidak ada respons di cache, ambil dari server
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('notificationclick', function(event) {
-  // Menangani saat pengguna mengklik notifikasi
-  event.notification.close();
-  clients.openWindow('/');
-});
-
-self.addEventListener('install', function(event) {
-  // Menampilkan pesan untuk menginstal website setelah 60 detik
-  event.waitUntil(
-    setTimeout(function() {
-      self.registration.showNotification('Instal Aplikasi', {
-        body: 'Instal aplikasi kami untuk pengalaman membaca yang lebih baik.',
-        icon: '/android-chrome-512x512.png',
-        data: {
-          url: '/'
-        }
-      });
-    }, 60000) // 60 detik
-  );
-});
-            
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
