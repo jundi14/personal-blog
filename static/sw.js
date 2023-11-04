1bc4c7bd810e30a91782319d6abbaefb3980aeea
Jundi Mubarok <57140394+jundi14@users.noreply.github.com>
2023-11-05 05:30:21 +0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,18 +1,49 @@
-// This is the "Offline copy of pages" service worker
+// Nama cache
+const CACHE_NAME = 'my-pwa-cache-v1';
 
-const CACHE = "pwabuilder-offline";
+// Daftar file yang ingin Anda cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/main.bundle.min.fbc83612bd2fcc6752eb12b055551f67f86a6b7216aa3264538229deb4b78e8d5bbcad44671b4d3fccd0c94b940472716aed6c4c915.css',
+  // Tambahkan file-file lain yang ingin Anda cache di sini
+];
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// Instalasi service worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+// Menggunakan cache saat offline
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Menghapus cache yang sudah kadaluarsa
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+               
