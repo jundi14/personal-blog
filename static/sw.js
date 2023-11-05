b5cf7a0a289d53a4b1f77753c011a394a61bd65c
Jundi Mubarok <57140394+jundi14@users.noreply.github.com>
2023-11-06 05:35:37 +0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,25 +1,16 @@
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js')
-        .then((registration) => {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
-          console.error('Service Worker registration failed:', error);
-        });
-    }
+const CACHE = "pwabuilder-offline";
 
-    if ('Notification' in window) {
-      Notification.requestPermission()
-        .then((permission) => {
-          if (permission === 'granted') {
-            const notification = new Notification('Instal Aplikasi', {
-              body: 'Instal aplikasi untuk pengalaman terbaik',
-              icon: '/android-chrome-512x512.png'
-            });
-          }
-        });
-    }
-  }, 5000); // Menampilkan notifikasi setelah 5 detik
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
