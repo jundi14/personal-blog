38d0ea14ef4b4854c8e907306fe608d8a83b26a9
Jundi Mubarok <57140394+jundi14@users.noreply.github.com>
2023-11-06 05:25:40 +0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,51 +1,25 @@
-// Nama cache
-const CACHE_NAME = 'my-pwa-cache-v1';
-
-// Daftar file yang ingin Anda cache
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/main.bundle.min.fbc83612bd2fcc6752eb12b055551f67f86a6b7216aa3264538229deb4b78e8d5bbcad44671b4d3fccd0c94b940472716aed6c4c915.css',
-  '/fonts/ibmplexsans_regular.otf',
-  '/js/*.js'
-  // Tambahkan file-file lain yang ingin Anda cache di sini
-];
-
-// Instalasi service worker
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Menggunakan cache saat offline
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Menghapus cache yang sudah kadaluarsa
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(name) {
-          return name !== CACHE_NAME;
-        }).map(function(name) {
-          return caches.delete(name);
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
         })
-      );
-    })
-  );
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    }
+
+    if ('Notification' in window) {
+      Notification.requestPermission()
+        .then((permission) => {
+          if (permission === 'granted') {
+            const notification = new Notification('Instal Aplikasi', {
+              body: 'Instal aplikasi untuk pengalaman terbaik',
+              icon: '/android-chrome-512x512.png'
+            });
+          }
+        });
+    }
+  }, 5000); // Menampilkan notifikasi setelah 5 detik
 });
-               
